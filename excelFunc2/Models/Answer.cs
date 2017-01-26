5a064e19fce680b84b14e7ca4d57d85b8b634df3
DESKTOP-DSRH9A9\ADSIXPS15 <zcao@goadsi.com>
2017-01-26 11:53:42 -0500

    create question and answer models

---
+++ excelFunc2/Models/Answer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace excelFunc2.Models
+{
+    public class Answer
+    {
+        int ID { get; set; }
+
+        string Cotent { get; set; }
+
+        string Explaination { get; set; }
+
+        int Answer_Flag { get; set; }
+
+    }
+}
---
+++ excelFunc2/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace excelFunc2.Models
+{
+    public class Question
+    {
+        int ID { get; set; }
+
+        string Content { get; set; }
+
+        int Difficulty { get; set; }
+
+        int NumberOfWrongGlobal { get; set; }
+
+        int NumberOfCorrectGlobal { get; set; }
+
+    }
+}
